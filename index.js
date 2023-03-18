c5f80fa5b21dd59eb269520a1697adc232efc285
Divya Suriakumar <20cs050@kpriet.ac.in>
2023-03-18 22:06:15 +0530

    link

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
-// app.use("https://blogspot-api.onrender.com/api/auth", authRoute);
-// app.use("https://blogspot-api.onrender.com/api/users", userRoute);
-// app.use("https://blogspot-api.onrender.com/api/posts", postRoute);
-// app.use("https://blogspot-api.onrender.com/api/categories", categoryRoute);
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/categories", categoryRoute);
+app.use("https://blogspot-api.onrender.com/api/auth", authRoute);
+app.use("https://blogspot-api.onrender.com/api/users", userRoute);
+app.use("https://blogspot-api.onrender.com/api/posts", postRoute);
+app.use("https://blogspot-api.onrender.com/api/categories", categoryRoute);
 
 // app.listen("5000", () => {
 //   console.log("Backend is running.");
